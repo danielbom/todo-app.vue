ab8803714335b59049669c374367432f876947cb
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-24 01:25:59 -0300

    feat: Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import router from "./router";
+
+// Extern libraries
+import "@fortawesome/fontawesome-free/js/all";
+import "jquery/dist/jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import moment from "moment";
+moment.locale("pt-br");
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
