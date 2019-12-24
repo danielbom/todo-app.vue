2400a444ff727ec024005861c79178978fc4b0c5
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-24 14:56:09 -0300

    feat: Update page Home
    Add component FilterTask

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,56 +4,7 @@
     <div class="col-lg-3 col-md-6 col-sm-12 p-1 pt-2">
       <CreateTask :clients="clients" :colaborators="colaborators"></CreateTask>
       <!-- Card filters -->
-      <div class="card">
-        <!-- Card filters header -->
-        <div class="card-header" v-on:click="showFilter = !showFilter">
-          APLICAR FILTROS
-        </div>
-        <!-- Card filters body -->
-        <div class="card-body" v-show="showFilter">
-          <form>
-            <!-- Card filters group task -->
-            <div class="form-group">
-              <label>Tarefa</label>
-              <input
-                type="text"
-                class="form-control"
-                v-model="filters.title"
-                placeholder="Título"
-              />
-            </div>
-            <!-- Card filters group task -->
-            <div class="form-group">
-              <label>Colaborador</label>
-              <select class="form-control" v-model="filters.colaborator">
-                <option value="">Todos</option>
-                <option
-                  v-for="c in colaborators"
-                  :value="c.colaboratorId"
-                  :key="c.key"
-                >
-                  {{ c.nome }}
-                </option>
-              </select>
-            </div>
-            <!-- Card filters group client -->
-            <div class="form-group">
-              <label>Cliente</label>
-              <select class="form-control" v-model="filters.client">
-                <option value="">Todos</option>
-                <option
-                  v-for="c in clients"
-                  v-show="c.status === 'ativo'"
-                  :value="c.clientId"
-                  :key="c['.key']"
-                >
-                  {{ c.nome }}
-                </option>
-              </select>
-            </div>
-          </form>
-        </div>
-      </div>
+      <FilterTask></FilterTask>
     </div>
     <!-- Second col: Todo list -->
     <div class="col-lg-3 col-md-6 col-sm-12 p-1 pt-2">
@@ -145,58 +96,25 @@
 </template>
 
 <script>
-import { database } from "../database";
+import { mapState } from "vuex";
 
 import CardTask from "@/components/CardTask.vue";
 import CreateTask from "@/components/CreateTask.vue";
+import FilterTask from "@/components/FilterTask.vue";
 
-/* eslint-disable no-console */
 export default {
   components: {
     CardTask,
-    CreateTask
-  },
-
-  data() {
-    return {
-      showFilter: false,
-
-      allTasks: [],
-      clients: [],
-      colaborators: [],
-
-      filters: {
-        title: "",
-        colaborator: "",
-        client: "",
-        by: {
-          colaborator: task => task.colaboratorId === this.filters.client,
-          client: task => task.clientId === this.filters.client,
-          title: task =>
-            task.title.toLowerCase().includes(this.filters.title.toLowerCase())
-        }
-      }
-    };
+    CreateTask,
+    FilterTask
   },
 
   computed: {
-    tasks() {
-      let tasks = this.allTasks;
-
-      if (this.filters.title.length > 0) {
-        tasks = tasks.filter(this.filters.by.title);
-      }
-      if (this.filters.client.length > 0) {
-        tasks = tasks.filter(this.filters.by.client);
-      }
-      if (this.filters.colaborator.length > 0) {
-        tasks = tasks.filter(this.filters.by.colaborator);
-      }
-
-      return tasks
-        .sort((t1, t2) => t1.time > t2.time) // asc
-        .sort((t1, t2) => t1.importance < t2.importance); // desc
-    },
+    ...mapState({
+      tasks: state => state.tasks.filtered,
+      clients: state => state.clients.all,
+      colaborators: state => state.colaborators.all
+    }),
     // computed by state
     doneTasks() {
       // Recently completed
@@ -231,7 +149,7 @@ export default {
 
       return this.todoTasks.filter(({ time }) => time > tomorrow);
     }
-  },
+  }
 };
 </script>
 
