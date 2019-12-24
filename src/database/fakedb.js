638da17c7a91b4ecea370f94655c96b4241fa82a
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-24 18:00:35 -0300

    feat: Update fakedb

--- src/database/fakedb.js
+++ src/database/fakedb.js
@@ -1,34 +1,107 @@
-/* eslint-disable no-console */
+/* eslint-disable */
+
 const DATA = {
   tasks: [
     {
-      ".key": 1,
-      title: "Test title",
-      detail: "Test detail",
-      clientName: "Me",
-      clientId: 1,
-      colaboratorName: "Other",
-      colaboratorId: 1,
-      importance: 2,
+      id: 1,
+      title: "Test title 1",
+      detail: "Test detail 1",
+      client: {
+        id: 1,
+        name: "One"
+      },
+      colaborator: {
+        id: 2,
+        name: "Two"
+      },
+      importance: "high",
       status: "todo",
       createdAt: new Date(2019, 12, 10),
       time: new Date(2019, 12, 12)
     },
     {
-      ".key": 1,
-      title: "Test title",
-      detail: "Test detail",
-      clientName: "Me",
-      clientId: 1,
-      colaboratorName: "Other",
-      colaboratorId: 1,
-      importance: 2,
-      status: "todo",
+      id: 2,
+      title: "Test title 2",
+      detail: "Test detail 2",
+      client: {
+        id: 1,
+        name: "One"
+      },
+      colaborator: {
+        id: 2,
+        name: "Two"
+      },
+      status: "doing",
+      createdAt: new Date(2019, 12, 10),
+      time: new Date(2019, 12, 12)
+    },
+    {
+      id: 3,
+      title: "Test title 3",
+      detail: "Test detail 3",
+      client: {
+        id: 1,
+        name: "One"
+      },
+      colaborator: {
+        id: 2,
+        name: "Two"
+      },
+      status: "done",
       createdAt: new Date(2019, 12, 10),
       time: new Date(2019, 12, 12)
     }
   ],
-  clients: []
+  clients: [
+    {
+      id: 1,
+      name: "One",
+      scheme: "initial",
+      status: "active"
+    },
+    {
+      id: 2,
+      name: "Two",
+      scheme: "intermediary",
+      status: "active"
+    },
+    {
+      id: 3,
+      name: "Three",
+      scheme: "complete",
+      status: "active"
+    },
+    {
+      id: 4,
+      name: "Four",
+      scheme: "initial",
+      status: "inactive"
+    }
+  ],
+  colaborators: [
+    [
+      {
+        id: 1,
+        name: "One",
+        status: "active"
+      },
+      {
+        id: 2,
+        name: "Two",
+        status: "active"
+      },
+      {
+        id: 3,
+        name: "Three",
+        status: "active"
+      },
+      {
+        id: 4,
+        name: "Four",
+        status: "inactive"
+      }
+    ]
+  ]
 };
 
 const fakedb = {
